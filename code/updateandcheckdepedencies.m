8e370fbd72aba00ef38c0456b339c5e4878f105f
Roee Gilron <roee@RoeeG.local>
2019-08-23 14:58:04 -0700

    all changes

--- code/updateandcheckdepedencies.m
+++ code/updateandcheckdepedencies.m
@@ -4,9 +4,11 @@ system('git pull origin master');
 
 % check if you have turtle json installed 
 if ~exist(fullfile(pwd,'toolboxes','turtle_json'),'dir')
+    mkdir(fullfile(pwd,'toolboxes'));
     cd(fullfile(pwd,'toolboxes'));
     system('git clone https://github.com/JimHokanson/turtle_json.git');
     cd('..');
     addpath(genpath(fullfile(pwd,'toolboxes')));
+    addpath(genpath(fullfile(pwd)));
 end
 end
