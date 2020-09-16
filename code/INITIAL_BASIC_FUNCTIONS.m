a7f320586293dd52e43bdb2abffdb09fc150108d
Juan Anso <juan.anso@gmail.com>
2020-09-16 11:22:59 -0700

    adding practice script used for demoing how to use code

--- code/INITIAL_BASIC_FUNCTIONS.m
+++ code/INITIAL_BASIC_FUNCTIONS.m
@@ -1,15 +1,21 @@
 %% Pipeline functions for basic manipulation of RCS data
 
+close all; clear all; clc
+
+% path to RCS data
+PATH_TO_SESSION_FOLDER = '/Users/juananso/Dropbox (Personal)/Work/DATA/RCS_patients/RCS09/09-15-2020_dataPoint_titration_0.1mA/SCBS/RCS09L';
+
 % to add toolboxes with basic functions and keep a simple root directory
 % 'toolboxes' contains the basic needed functions to extract the data
+% includeToolbox
 addpath(genpath(fullfile(pwd,'toolboxes')));
 
 % transforms .json to .mat files and gives database overview in folder
-MAIN_load_rcs_data_from_folder(PATH_TO_SESSION_FOLDER)
-MAIN_load_rcsdata_from_folders(PATH_TO_SESSIONS_FOLDER)
+MAIN_load_rcsdata_from_folders(PATH_TO_SESSION_FOLDER)
 
 % read device settings
-loadDeviceSettings(PATH_TO_DEVICESETTINGS_JSON)
+deviceSettingsPaths = findFilesBVQX(PATH_TO_SESSION_FOLDER,'DeviceSettings.json');
+devicesettings = loadDeviceSettings(char(deviceSettingsPaths(1)))
 
 % read event logs
 loadEventLog(PATH_TO_EVENTLOG_JSON)
