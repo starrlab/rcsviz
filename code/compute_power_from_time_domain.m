357d4e8441904b7c29b38fa3433d46fcb8f42a22
Juan Anso <juan.anso@gmail.com>
2020-12-02 15:36:07 -0800

    FIX SCALING FFT
    - bin is now taken as integer number bin
    - fft is now modified to transform to to single sided
    - fft magnitude is plotted
    - fft final scale is weighted using 1/2 number of samples of fft
    - power is scaled with a factor 2
    
    still unknown
    - why factor 1/2 on the fft final scale
    - why power is scaled with a factor of 2
    
    ...

--- code/compute_power_from_time_domain.m
+++ code/compute_power_from_time_domain.m
@@ -32,11 +32,12 @@ PB1 = PowerData.Band1;
 tpwr = PowerData.DerivedTime;
 tpwrs = seconds(tpwr-tpwr(1))/1000;
 interval_ms = powerSettings.fftConfig.interval; % is given in ms
-fftBins = powerSettings.powerBandsInHz.fftBins;
-fftBinUpperBound = powerSettings.powerBandsInHz.upperBound(1); % assuming only 1 power band (B1)
-fftBinLowerBound = powerSettings.powerBandsInHz.lowerBound(1);
-fUpperBin = fftBins(max(find(fftBins<fftBinUpperBound)));
-fLowerBin = fftBins(max(find(fftBins<fftBinLowerBound)));
+
+fftBinsHz = powerSettings.powerBandsInHz.fftBins;
+binStart = powerSettings.powerBands{1}.band0Start;
+binStart(1) = binStart(1)+1; % not clear why but it gives closer results
+binEnd = powerSettings.powerBands{1}.band0Stop;
+binEnd(1) = binEnd(1)+1; % not clear why but it gives closer results
 fftSize = powerSettings.powerBandsInHz.fftSize;
 sr = timeDomainSettings.samplingRate;
 
@@ -51,15 +52,20 @@ end
 overlap = 1-(sr*interval_ms/1e3/fftpt_overlapcalc);
 timewin = fftSize/sr;
 tpwrs = tpwrs + seconds(timewin); % add offset of first power sample after first fft
-timewinidx = round(timewin*sr); % convert ms to idx
+L = fftSize; % timeWin is now named L, number of time window points
 
 % create hann taper function, equivalent to the Hann 100% 
-hann_win = .5*(1-cos(2*pi*(0:timewinidx-1)/(timewinidx-1)));
+hann_win = 0.5*(1-cos(2*pi*(0:L-1)/(L-1)));
+figure(1)
+plot(hann_win,'-o')
+title('Hann Window (100%)')
+xlabel('samples')
+ylabel('scale 0 to 1')
 
 % plot the data
-fig1 = figure(1); fontSize = 16;
+fig2 = figure(2); fontSize = 16;
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [1/2, 0, 1/2, 1]);
-set( findall(fig1, '-property', 'fontsize'), 'fontsize', fontSize)
+set( findall(fig2, '-property', 'fontsize'), 'fontsize', fontSize)
 
 % plot benchtop time domain signal
 subplot(411)
@@ -69,19 +75,27 @@ title('One trial of data')
 % output fft parameter
 subplot(424), hold on
 axis off
-text(0,1,['sr = ', num2str(sr), 'Hz'])
-text(0,0.9,['fftsize = ', num2str(fftSize), 'pnts'])
-text(0,0.8,['fft time window = ', num2str(timewin), 'ms'])
-text(0,0.7,['fft interval = ', num2str(interval_ms), 'ms'])
-text(0,0.6,['fft overlap = ', num2str(overlap*100), '%'])
-text(0.5,1,['fUpperBin = ', num2str(fUpperBin), 'Hz'])
-text(0.5,0.9,['fLowerBin = ', num2str(fLowerBin), 'Hz'])
+startCol1 = -0.2;
+startY = 0.9;
+% first col
+text(startCol1,startY+0.1,'FFT settings')
+text(startCol1,startY,['sr = ', num2str(sr), 'Hz'])
+text(startCol1,startY-.1,['fftsize = ', num2str(L), 'pnts'])
+text(startCol1,startY-.2,['fft time window = ', num2str(timewin*1e3), 'ms'])
+text(startCol1,startY-.3,['fft interval = ', num2str(interval_ms), 'ms'])
+text(startCol1,startY-.4,['fft overlap = ', num2str(overlap*100), '%'])
+% second col
+startCol2 = startCol1+0.5;
+text(startCol2,startY+0.1,'FFT Power Bins')
+text(startCol2,startY,['fLowerBin = ', num2str(fftBinsHz(binStart)), 'Hz'])
+text(startCol2,startY-0.1,['fUpperBin = ', num2str(fftBinsHz(binEnd)), 'Hz'])
+text(startCol2,startY-.2,['all bins avg Power= ', num2str(fftBinsHz(binStart:binEnd))])
 
 % plot next fft segment window and the hanned equivalent
 subplot(423), hold on
 stime = 1; % sample 1 of data set where window starts
-plot(t(stime:stime+timewinidx-1),lfp_td(stime:stime+timewinidx-1))
-plot(t(stime:stime+timewinidx-1),lfp_td(stime:stime+timewinidx-1)'.*hann_win,'r')
+plot(t(stime:stime+L-1),lfp_td(stime:stime+L-1))
+plot(t(stime:stime+L-1),lfp_td(stime:stime+L-1)'.*hann_win,'r')
 title('One short-time window of data, windowed')
 legend('raw lfp_td','lfp_td * hann')
 if transform2rcsUnits
@@ -89,104 +103,94 @@ if transform2rcsUnits
 else
     ylabel('voltage (uV)')
 end
-set( findall(fig1, '-property', 'fontsize'), 'fontsize', fontSize)
+set( findall(fig2, '-property', 'fontsize'), 'fontsize', fontSize)
 
 % plot power signal from RCS
 subplot(414)
 yyaxis left
 plot(tpwrs,PB1,'color','b','LineWidth',2)
-if transform2rcsUnits
-    ylabel('power (rcs au)')
-else
-    ylabel('power (uV^2)')
-end
+ylabel('power (rcs au)')
 title(['Mean Power Value from RCS = ',num2str(mean(PB1))])
 
 % loop get fft of window of data using time window increments
-numFFTWindows_calcfromTD = ceil(length(lfp_td)/timewinidx/(1-overlap))+1; 
+numFFTWindows_calcfromTD = ceil(length(lfp_td)/L/(1-overlap))+1; 
 for ii=1:numFFTWindows_calcfromTD
+    figure(2)
     % check at least one time window available before reach end signal
-    if stime+timewinidx <= length(t)        
+    if stime+L <= length(t)        
         % indicate where in time the running window is on time domain signal
         subplot(411), hold on
-        plot(t(stime+timewinidx),0,'ok')
+        plot(t(stime+L),0,'ok')
         if transform2rcsUnits
             ylabel('voltage (rcs au)')
         else
             ylabel('voltage (uV)')
         end
 
-        % plot raw and hanned version (blue) of the time windowed (red) signal segment
+        % plot of fft time window, raw (blue) and hanned (red) segments
         subplot(423), hold off
-        plot(t(stime:stime+timewinidx-1),lfp_td(stime:stime+timewinidx-1)), hold on
-        plot(t(stime:stime+timewinidx-1),lfp_td(stime:stime+timewinidx-1)'.*hann_win,'r')
+        plot(t(stime:stime+L-1),lfp_td(stime:stime+L-1)), hold on
+        plot(t(stime:stime+L-1),lfp_td(stime:stime+L-1)'.*hann_win,'r')
         title('time window for next fft')
         if transform2rcsUnits
             ylabel('voltage (rcs au)')
         else
             ylabel('voltage (uV)')
         end
+        legend('raw signal','hann windowed')
 
         % calculate average Power (squared fft of time windowed signal)
-        yfft = fft(lfp_td(stime:stime+timewinidx-1)'.*hann_win,fftSize);
-        fftPower = abs(yfft(1:floor(length(hann_win)/2)+1)).^2;
-        fiUp = find(fftBins==fUpperBin); % using frequency bins from fftBins = powerSettings.powerBandsInHz.fftBins;
-        fiLow = find(fftBins==fLowerBin);
-        avgPower(ii) = (1/length(fiLow:fiUp))*sum(fftPower(fiLow:fiUp));
+        X = fft(lfp_td(stime:stime+L-1)'.*hann_win,L);
+        SSB = X(1:L/2); % single sided
+        SSB(2:end) = 2*SSB(2:end); % scaling step 1, to go from Dobulbe Sided to Single Sided
+        YFFT = abs(SSB/(L/2)); % scaling step 2, dividing by (#?) time window length
+        fftPower = 2*(YFFT.^2);
+        avgPower(ii) = (1/length(binStart:binEnd))*sum(fftPower(binStart:binEnd));
         
         % plot calculated fft of last time window
         subplot(413)
-        yyaxis right, plot(fftBins,fftPower(1:end-1),'-+r');
-        title('power spectrum from time window'), xlabel('frequency (Hz)')
+        yyaxis right, plot(fftBinsHz(1:length(YFFT)),YFFT,'-+r');
+        title('fft last time window'), xlabel('frequency (Hz)')
         if transform2rcsUnits
-            ylabel('power (rcs au)')
+            ylabel('|X(f)| (rcs au)')
         else
-            ylabel('power (uV^2)')
+            ylabel('|X(f)| (uV^2)')
         end
         
         % superimpose actual fft from RCS
         if ii<=size(FFTData,1)
             yyaxis left
-            plot(fftBins,FFTData.FftOutput{ii},'-ob');
+            plot(fftBinsHz,FFTData.FftOutput{ii},'-ob');
             ylabel('fft (rcs au)')
             xlabel('frequency (Hz)')
             legend('fft rcs','fft calculated')
         end
 
         % plot caculated power (red +) on same pannel as RCS (magenta)
-        subplot(414)
-        yyaxis right
-        plot(tpwrs(1:ii),avgPower(1:ii),'-+r')
-        if transform2rcsUnits
-            ylabel('power (rcs au)')
-        else
-            ylabel('power (uV^2)')
-        end
+        if ii<=length(tpwrs)
+            subplot(414)
+            yyaxis right
+            plot(tpwrs(1:ii),avgPower(1:ii),'-r','LineWidth',2)
 
-        set( findall(fig1, '-property', 'fontsize'), 'fontsize', fontSize)    
+            if transform2rcsUnits
+                ylabel('power (rcs au)')
+            else
+                ylabel('power (uV^2)')
+            end
+            legend('power rcs','power calculated')
+        end
     else
         % do nothing
     end   
-    stime = stime + (timewinidx - ceil(timewinidx*overlap));
+    set( findall(fig2, '-property', 'fontsize'), 'fontsize', fontSize)    
+    stime = stime + (L - ceil(L*overlap));
     pause(0.1)
 end
-
-% display the ration between mean Power RCS and calculated power from TD
-disp(['mean power from RCS = ',num2str(mean(PB1))])
-disp(['mean calculated power = ',num2str(mean(avgPower))])
-disp(['Ratio between RCS power and calculate = ',num2str(mean(PB1)/mean(avgPower))])
-
-legend(['power from RCS (mean) = ',num2str(num2str(mean(PB1)))],...
-            ['power calculated (mean) = ',num2str(mean(avgPower))])
+legend(['avg power rcs = ',num2str(num2str(mean(PB1)))],...
+       ['avg power calculated = ',num2str(mean(avgPower))])
 title(['power rcs / power calculated = ', num2str(mean(PB1)/mean(avgPower))])
 
-% plot all FFT data from INS without any scaling in a different figure
-if ~isempty(FFTData)
-    numFFTs = size(FFTData,1);
-    figure(3), hold on
-    for ii=1:numFFTs
-        plot(fftBins,FFTData.FftOutput{ii},'o-')    % 1e6 to go from power in mV2 to uV2
-    end
-end
-title('raw fft from RCS in au')
-xlabel('frequency (Hz)')
+% display the ration between mean Power RCS and calculated power from TD
+disp(['avg power rcs = ',num2str(mean(PB1))])
+disp(['avg power calculated = ',num2str(mean(avgPower))])
+disp(['Ratio rcs to calculated = ',num2str(mean(PB1)/mean(avgPower))])
